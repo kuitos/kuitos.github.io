aed37ae0e31377830c7dfbb82dc3e76c6a938bd1
Kuitos <kuitos.lau@gmail.com>
2015-12-04 15:32:35 +0800

    update deploy script

--- webpack-build.config.js
+++ webpack-build.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   output : {
     path             : path.join(__dirname, 'build'),
-    publicPath       : '/kuitos.github.io/build/',
+    publicPath       : 'build/',
     filename         : '[name]-[hash:8].js'
   },
   plugins: [
